587ccf099047272f43e656755d3a2e8d5db8d5fe
sumagowri <64245422+sumagowri@users.noreply.github.com>
2022-12-19 10:14:47 +0530

    Initial

---
+++ audit-authorization/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (1, 'Nikhil Kumar', 'nk', 'nk000', 'Project-1');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (2, 'Rachita Bhasin', 'rb', 'rb000', 'Project-2');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (3, 'Mangesh Modokar', 'mm', 'mm000', 'Project-3');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (4, 'Nitu Sinha', 'ns', 'ns000', 'Project-4');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (5, 'Varsha Joshi', 'vj', 'vj000', 'Project-5');
